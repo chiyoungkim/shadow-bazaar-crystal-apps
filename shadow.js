3bbb065bf7af00d35b385e8b31fa851f713d6640
Chiyoung Kim <chiyoungkim95@gmail.com>
2024-05-17 09:07:11 -0400

    loading

---
+++ shadow.js
@@ -0,0 +1,27 @@
+const enterButton = document.getElementById("enter");
+
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        enterButton.classList.remove("hidden");
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+}
+move();
+
+enterButton.addEventListener("click", () => {
+    window.location.href = "/index.html";
+});
